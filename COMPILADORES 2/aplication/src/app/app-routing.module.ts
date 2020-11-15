50e17041237c61653883dbb8711f09d6d6cd4472
PabloAndresArg <PabloAndresArg@gmail.com>
2020-11-15 01:10:23 -0600

    ok
    ok

--- COMPILADORES 2/aplication/src/app/app-routing.module.ts
+++ COMPILADORES 2/aplication/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntradaComponent } from './components/entrada/entrada.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'entrada', pathMatch: 'full' },
+  {path: 'entrada', component: EntradaComponent},
+  {path:'**', redirectTo: 'entrada'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
