8e49a8bc7a6a2e2e30f319534fe629f6c7a375db
PabloAndresArg <PabloAndresArg@gmail.com>
2020-11-16 18:50:07 -0600

    ok
    .

--- COMPILADORES 2/aplication/src/app/components/entrada/entrada.component.ts
+++ COMPILADORES 2/aplication/src/app/components/entrada/entrada.component.ts
@@ -1,37 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import jison from "./../../../GRAMATICA/olc2";
+import * as File_ from 'file-saver';
+import {saveAs} from 'file-saver';
 @Component({
   selector: 'app-entrada',
   templateUrl: './entrada.component.html',
   styleUrls: ['./entrada.component.css']
 })
 export class EntradaComponent implements OnInit {
-  entradaAnalizar: string;
-  id = 1; 
-  content: string; 
-  constructor() { 
+  public entradaAnalizar: string;
+  public id = 1; 
+  // -------------------
+  public contentFile: any;
+  public file:any; 
+  public unlocown:any; 
+  // -------------------
+  public entradas:any = [];
+  public cantidadPestanias: number = 9;
+  constructor() {
+    for (let i = 0 ; i < this.cantidadPestanias ; i++) {
+      this.entradas.push('\t');
+    } 
+   
   }
 
   ngOnInit(): void {
+    for (let i = 1 ; i < this.cantidadPestanias ; i++) {
+      this.entradas[i] ='\t';
+    } 
   }
 
-  public setText(formTextArea: any): any{
-    if (formTextArea.value.length == 1){
-      let aux = formTextArea.value;
-      formTextArea.value = '\t';
-      formTextArea.value += aux;
-    }
-    this.entradaAnalizar = formTextArea.value;
-  }
   
-  public excuteProgram(formTextArea: any): any{
-    console.log('Analizando: ' , formTextArea.value);
-    let arbol = jison.parse('numeric string');
-    console.log(arbol);
+  public excuteProgram(entrada_: string): any{
+    console.log('Analizando: ' , entrada_);
+    try {
+      let arbol = jison.parse(entrada_);
+      console.log(arbol);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   public addWindow(): any{
-    if(this.id < 9 ){
+    if(this.id < this.cantidadPestanias){
       let padreWindows = document.getElementById('padreWindows');
       // preparando el nuevo elemento
       let nuevoLI = document.createElement('li');
@@ -48,6 +59,32 @@ export class EntradaComponent implements OnInit {
         console.log(a.getAttribute('href'));
     }
   }
+
+
+  // .................. ARCHIVOS 
+
+  SA_VE(indice: any){
+    try{ 
+    let entrada_save = new Blob([ this.entradas[indice] ], { type: "text/csv;charset=utf-8" });
+    saveAs(entrada_save, "Entrada_.txt");
+    alert("Entrada guardada correctamente");
+  }catch(e){
+    alert("algo salio mal..");
+  }
+  }
+
+  fileChanged(e , indice:any) {
+  this.file = e.target.files[0];
+  this.uploadDocument(indice);
+  }
+  uploadDocument(indice : any){
+  let fileReader = new FileReader(); 
+  fileReader.onload = (e) =>{
+  this.entradas[indice] = fileReader.result;
+  }
+  fileReader.readAsText(this.file);
+  }
+
 
 
 
